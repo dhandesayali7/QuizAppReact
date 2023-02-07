ba4e3ada459f75e83841d2f49cf308856c7690dc
dhandesayali7 <119158423+dhandesayali7@users.noreply.github.com>
2023-02-07 10:05:18 +0530

    React

---
+++ src/Helper/QuestionBank.js
@@ -0,0 +1,76 @@
+export const questions = [
+  {
+    prompt:
+      "Which symbol shows up in output windows of QBasic to indicate that is expecting input?",
+    option1: "!",
+    option2: "?",
+    option3: "\\",
+    option4: "''",
+    answer: "A",
+  },
+  {
+    prompt:
+      "Controls a computer's tasks and manages system resources to optimize performance...",
+    option1: "Software",
+    option2: "Operating System",
+    option3: "Central Processing Unit",
+    option4: "hardware",
+    answer: "B",
+  },
+  {
+    prompt:
+      "If a computer were a person, what is the brain?",
+    option1: "Optical Storage",
+    option2: "Application Software",
+    option3: "Central Processing Unit",
+    option4: "Read Only Memory",
+    answer: "C",
+  },
+  {
+    prompt:
+      "Which of the following are part of a computer system? (check all that apply)",
+    option1: "Input",
+    option2: "Process",
+    option3: "Output",
+    option4: "Storage",
+    answer: "D",
+  },
+  {
+    prompt:
+      " A file containing relatively permanent data is…",
+    option1: "Random file",
+    option2: "Transaction file",
+    option3: "Master file",
+    option4: "Sequential file",
+    answer: "C",
+  },
+ 
+  
+    {
+      prompt:
+          " A computer in which data is represented by a continuously variable physical quantity is …..",
+        option1: "Digital computer",
+        option2: "Analog computer",
+        option3: "Hybrid computer",
+        option4: "Machine inbuilt computer",
+        answer: "A",
+      },
+      {
+        prompt:
+            " The operating system manages",
+          option1: "Memory",
+          option2: "Processes",
+          option3: "Disks and I/O devices",
+          option4: "All of the above",
+          answer: "D",
+        },
+        {
+          prompt:
+              "The only function of a not gate is to ….. ",
+            option1: "stop a signal",
+            option2:"re-complement a signal",
+            option3: "invert an output signal",
+            option4: "act as a universal gate",
+            answer: "C",
+          },
+];
